fd160ff615a019538c4d8320d04801323fef7e70
Omar Alaa Elnahass <21p0197@eng.asu.edu.eg>
2025-04-19 01:04:29 +0200

    add: structure for frontend and backend

---
+++ E-Commerce-Backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json()); // for JSON payloads
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+
+// Error Handlers
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ E-Commerce-Frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import {useContext} from "react";
+import "./Navbar.css"
+
+// Definitions (Functions, Variables, Constants, etc.)
+
+const Navbar = () => {
+    // Logic Code
+    return (
+        <>
+            <h1>Navbar</h1>
+        </>
+    )
+}
+
+export default Navbar;

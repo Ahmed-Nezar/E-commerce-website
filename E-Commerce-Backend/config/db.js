5383c9cdca4153888d5b9124d5f45c9f67f88120
Omar Alaa Elnahass <21p0197@eng.asu.edu.eg>
2025-04-22 19:37:32 +0200

    remove node_modules

--- E-Commerce-Backend/config/db.js
+++ E-Commerce-Backend/config/db.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function connectDB() {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+        // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+        await mongoose.connect(process.env.MONGO_URI, clientOptions);
+        await mongoose.connection.db.admin().command({ ping: 1 });
+        console.log("Connected to MongoDB (Online - Cloud)");
+    } catch (e) {
+        throw new Error("Error connecting to MongoDB (Online - Cloud)");
     }
-};
+}
 
-module.exports = connectDB;
+connectDB().catch(e => console.error(e));

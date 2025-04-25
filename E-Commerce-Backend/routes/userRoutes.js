b97dacca7be5256903b60b05ada5151f733f100b
Ahmed-Nezar <ahmednezarrr@gmail.com>
2025-04-26 00:36:01 +0300

    refactor [backend]: restructure user routes to include token verification and controller methods

--- E-Commerce-Backend/routes/userRoutes.js
+++ E-Commerce-Backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
+const VerifyToken = require('../middlewares/VerifyToken');
 
-router.get('/profile', (req, res) => {
-    res.send('User profile');
-});
+// Routes
+router.get('/getProfile', VerifyToken("user"), userController.getUserProfile);
+router.put('/updateProfile', VerifyToken("user"), userController.updateProfile);
+router.put('/:id', VerifyToken("admin"), userController.updateUser); // Admin updates any user
+router.get('/getAll', VerifyToken("admin"), userController.getAllUsers); // Admin gets all users
+router.delete('/:id', VerifyToken("admin"), userController.deleteUser); // Admin deletes a user
 
 module.exports = router;
-

49fa57f2cc9e88cab054c81b23595170ebdffca7
AbdulRahman Hesham <21P0153@eng.asu.edu.eg>
2025-05-03 04:19:49 +0300

    feat [backend]: implement coupon routes with token verification and admin access for CRUD operations

--- E-Commerce-Backend/routes/couponRoutes.js
+++ E-Commerce-Backend/routes/couponRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const couponController = require('../controllers/couponController');
+const verifyToken = require('../middlewares/VerifyToken');
 
-router.get('/coupon', (req, res) => {
-    res.send('Coupon Router');
-});
+router.get('/', verifyToken(), couponController.getCoupons);
+router.post('/create', verifyToken('admin'), couponController.createCoupon);
+router.put('/:id', verifyToken('admin'), couponController.updateCoupon);
+router.delete('/:id', verifyToken('admin'), couponController.deleteCoupon);
 
 module.exports = router;
 
--- E-Commerce-Backend/routes/reviewRoutes.js
+++ E-Commerce-Backend/routes/reviewRoutes.js
@@ -5,6 +5,7 @@ const verifyToken = require('../middlewares/VerifyToken');
 
 router.post('/create', verifyToken(), reviewController.createReview);
 router.get('/get/:productId', reviewController.getReviews);
+router.get('/all', verifyToken('admin'), reviewController.getAllReviews);
 router.put('/update/:reviewId', verifyToken(), reviewController.updateReview);
 router.delete('/delete/:reviewId', verifyToken(), reviewController.deleteReview);
 
